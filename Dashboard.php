39db32322cab771e4787c82069cfb86a8c7e9595
MEENAL SHIKHA <MEENALSHIKHAS@GMAIL.COM>
2025-07-18 17:59:26 +0530

    Dashboard.php

---
+++ Dashboard.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EduTrack Dashboard</title>
+       <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css"  rel="stylesheet">
+</head>
+<body>
+  <div class="back">
+   <div class="header">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">EduTrack</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+     <nav class="navbar bg-body-tertiary">
+  <div class="con">
+    <a class="navbar-brand" href="#">
+      <img src="logo.jpg" alt="Bootstrap" width="30" height="24">
+    </a>
+  </div>
+</nav>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">logout</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Contact Us</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">About Us</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+   </div> 
+<div class="main">
+     <div class="my-row1">
+    <div class="box">
+      <img src="attend.png" alt="Cover 1" class="box-img" href="">
+     <a href="" class="box-heading">Attendence</a> 
+    </div>
+    <div class="box">
+       <img src="exam.jpg" alt="Cover 1" class="box-img">
+     <a href="" class="box-heading">Admit Card</a> 
+    </div>
+    <div class="box">
+       <img src="Course.png" alt="Cover 1" class="box-img">
+     <a href="" class="box-heading">Courses </a> 
+    </div>
+  </div>
+  <br>
+  <div class="my-row2">
+    <div class="box">
+       <img src="edit.png" alt="Cover 1" class="box-img">
+     <a href="edit.php" class="box-heading">Edit Profile</a> 
+    </div>
+    <div class="box">
+      <img src="assign.jpg" alt="Cover 1" class="box-img">
+     <a href="" class="box-heading">Assignments</a> 
+    </div>
+    <div class="box">
+       <img src="gradecard.png" alt="Cover 1" class="box-img">
+     <a href="" class="box-heading">Gradecard</a> 
+    </div>
+  </div>
+</div>
+</div>
+</div>
+</body>
+</html>
